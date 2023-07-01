c052bfaa1bc624578bddf4b5acb18c0c6a4a35fe
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-07-01 14:37:07 +0300

    add files project

---
+++ my-app/src/components/Counter/Counter.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import './CounterStyled.css';
+
+
+interface IState {
+  count: number;
+}
+
+const Counter: React.FC = () => {
+  const [state, setState] = useState<IState>({ count: 0 });
+
+  const increment = (): void => {
+    setState({ count: state.count + 1 });
+  };
+
+  const decrement = (): void => {
+    if(state.count <= 0) {
+        return;
+    }
+    setState({ count: state.count - 1 });
+  };
+
+
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">Counter</h1>
+        <p className="counter-num">{state.count}</p>
+        <button type="button" onClick={increment}>+</button>
+        <button type="button" onClick={decrement}>-</button>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
